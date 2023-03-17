f35850b33d82aec1b18b92594f58e89c2c8a8b06
thiagoFirsen <thiago.firsen@rethink.dev>
2023-03-17 16:47:38 -0300

    att

---
+++ Algoritmo e lógica de programação/Fundamentos/Inicio/comentario.js
@@ -0,0 +1,14 @@
+//Usado para comentarios - Ela não é interpretada pelo programa
+console.log("Passo 1");
+console.log("Passo 2"); //Segunddo passo - É possivel comentar na linha do codigo
+// console.log("Passo 3");
+
+/*
+ *
+ *   COMENTARIO
+ *   DE
+ *   MULTIPLAS
+ *   LINHAS
+ *
+ *
+ */
